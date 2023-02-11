deacc817924b96c01614a3cdd476f801a2794b3f
ArrmyFathah <125104884+Recotop@users.noreply.github.com>
2023-02-12 00:10:50 +0700

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -4,34 +4,33 @@
 
 /* sticky navbar */
 
-window.addEventListener('scroll', function()){
+window.addEventlistener('scroll', function() {
     var header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > header 0);
-}
+    header.classlist.toggle('sticky', window.scrollY > 0);
+});
 
 /* filterable cards */
-let list = document.querySelector('list');
-let list = document.querySelector('card');
 
-for(let i = 0; i<list.length; i++){
-    list[i].addEventListener('click', function()){
-        for(let j=0; j<list.length; j++){
-            list[j].addEventListener('click', function(){
+let.list = document.querySelectorAll('.list');
+let cards = document.querySelectorAll('.card');
+
+for(let i=0; i<list.length; i++) {
+    list[i].addEventListener('click', function() {
+        for(let j=0; j<list.length; j++) {
+            list[j].classList.remove('active');
         }
         this.classList.add('active');
-
-        let dataFilter = this.getAttribute('data-filter');
-
-        for(let k=0; k<card.length; k++)
-        {
+        
+        let dataFilter =this.getAttribute('data-filter');
+        
+        for( let k=0; k<cards.length; k++) {
             card[k].classList.remove('active');
             card[k].classList.add('hide');
 
-            if(card[k].getAttribute('data-item') == dataFilter || dataFilter == 'all'){
+            if(cards[k].getAttribute('data-item') == dataFilter || dataFilter == 'all') {
                 card[k].classList.remove('hide');
-                card[k].classList.add('active');
-                }
+                card[k].classList.add('active');                
             }
         }
-    }
+    })
 }
